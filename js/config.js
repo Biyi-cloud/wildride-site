d68e428cd8e799a67447c1931194ab6551f3629e
Biyi-cloud <ojoadebiyi7@gmail.com>
2025-02-21 10:42:27 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_lTtuKU0fD', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5ogfearijnfu5dt8qdtp9b154g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
