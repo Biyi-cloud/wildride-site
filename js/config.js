d34c0016b6f2768c808432b0d3e1141e24401c21
Biyi-cloud <ojoadebiyi7@gmail.com>
2025-02-23 17:54:50 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_lTtuKU0fD', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5ogfearijnfu5dt8qdtp9b154g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_xowHqUsVt', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6dlpkregdfqfnaeodjbk6h8r4q', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
